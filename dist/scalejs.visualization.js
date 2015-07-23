80d9ce904377d345653519ed74f3328ed6b77a3f
Peter Fatyga <peter.fatyga@eikospartners.com>
2015-07-23 10:59:14 -0400

    Updates readme Fixes bug where deep clone would not clone properties that were functions

--- dist/scalejs.visualization.js
+++ dist/scalejs.visualization.js
@@ -60,6 +60,32 @@ define('treemap/treemap.js',[
     core.mvvm.registerBindings(bindings);
     registerTemplates(template);
 
+    //http://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-clone-an-object#answer-1042676
+    function extend(from, to)
+    {
+        if (from === null || typeof from !== 'object') {
+            return from;
+        }
+        if (from.constructor !== Object && from.constructor !== Array) {
+            return from;
+        }
+        if (from.constructor == Date || from.constructor == RegExp || from.constructor == Function ||
+            from.constructor == String || from.constructor == Number || from.constructor == Boolean) {
+            return new from.constructor(from);
+        }
+
+        to = to || new from.constructor();
+
+        for (var name in from)
+        {
+            if(from.hasOwnProperty(name)) {
+                to[name] = typeof to[name] == 'undefined' ? extend(from[name], null) : to[name];
+            }
+        }
+
+        return to;
+    }
+
     return {
         viewModel: function(params) {
             this.data = ko.observable();
@@ -67,7 +93,8 @@ define('treemap/treemap.js',[
 
             ko.computed(function () {
                 //deep clone params.data because array order was being modified
-                var data = ko.unwrap(JSON.parse(JSON.stringify(params.data)));
+                var data = {};
+                extend(params.data, data);
                 d3.layout.treemap()
                     .round(false)
                     .sticky(true)
--- readme.md
+++ readme.md
@@ -7,34 +7,33 @@ component: {
     name: 'treemap',
     params: {
         data: {
-         "name": "flare",
-         "children": [
+         'name': 'flare',
+         'children': [
           {
-           "name": "analytics",
-           "children": [
+           'name': 'analytics',
+           'children': [
             {
-             "name": "cluster",
-             "children": [
-              {"name": "AgglomerativeCluster", "size": 3938},
-              {"name": "CommunityStructure", "size": 3812},
-              {"name": "MergeEdge", "size": 743}
+             'name': 'cluster',
+             'children': [
+              {'name': 'AgglomerativeCluster', 'size': 3938},
+              {'name': 'CommunityStructure', 'size': 3812},
+              {'name': 'MergeEdge', 'size': 743}
              ]
             },
             {
-             "name": "graph",
-             "children": [
-              {"name": "BetweennessCentrality", "size": 3534},
-              {"name": "LinkDistance", "size": 5731}
+             'name': 'graph',
+             'children': [
+              {'name': 'BetweennessCentrality', 'size': 3534},
+              {'name': 'LinkDistance', 'size': 5731}
              ]
             }
            ]
           }
          ]
         },
-        nodeBindings: {
-            click: function () {
-                console.log("clicked!");
-            }
-        }
+        value: function (item) {
+            return item.value;
+        },
+        nodeTemplate: 'treemap_node_template'
     }
 }
